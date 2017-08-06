61460faacc1a9e42d42ecead1da4b645b5d9fc1f
gautamamber <ambergautam1@gmail.com>
2017-08-07 00:18:51 +0530

    A friendship day special

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from flask_mysqldb import MySQL
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('quotes.html')
+if __name__ == '__main__':
+    app.secret_key='secret123'
+    app.run(debug=True)
