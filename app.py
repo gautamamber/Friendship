2657259f5669e54a6025a1ca8f2c5cecf7274114
gautamamber <ambergautam1@gmail.com>
2017-08-08 22:48:03 +0530

    A friendship day special

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return render_template('home.html')
+@app.route('/qa')
+def qa():
+    return render_template('qa.html')
+@app.route('/facts')
+def facts():
+    return render_template('facts.html')
+@app.route('/aboutf')
+def aboutf():
     return render_template('aboutf.html')
 if __name__ == '__main__':
     app.secret_key='secret123'
